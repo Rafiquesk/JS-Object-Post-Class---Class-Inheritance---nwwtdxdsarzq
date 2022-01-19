f9d981b76417c3589709bd1b81c3e2136c7cb987
Rafiquesk <96195298+Rafiquesk@users.noreply.github.com>
2022-01-19 22:55:13 +0530

    string

--- src/components/circle.js
+++ src/components/circle.js
@@ -2,5 +2,13 @@
 
 // declare class
 
-
 // export class using module.exports
+const Shape = require("./shape").default;
+
+class Circle extends Shape {
+  constructor(color) {
+    super();
+  }
+  calculateArea() {}
+}
+module.exports = Circle;
